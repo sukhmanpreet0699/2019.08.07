43ba17246e143da0de0804ec4c33ee7565cec9f1
Sukhmanpreet Singh <748045@student.tor.loc>
2019-08-07 16:17:36 -0400

    third commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const site=http.createServer(function(req,res){
     console.log('hello World');
     res.setHeader('Content-type','text/html');
+    console.log(req.rawHeaders);
     res.end('<h1> Hello Class!!</h1>');
 });
 site.listen(3000);
